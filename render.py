a30a8d5da1b88a00ec5ad33f6c08d3a903988214
Rafid Aslam (refeed) <rafidteam@gmail.com>
2022-08-08 11:16:21 +0700

    Use base_url from env rather than hardcoding it
    For easier debugging in local and working links in GH Pages

--- render.py
+++ render.py
@@ -1,20 +1,21 @@
+import os
 import shutil
 
 from jinja2 import Environment, FileSystemLoader
+
+BASE_URL = os.getenv("BASE_URL", "")
 
 environment = Environment(loader=FileSystemLoader("templates"))
 stream_list_template = environment.get_template("stream_list.html.jinja2")
 topic_list_template = environment.get_template("topic_list.html.jinja2")
 topic_chat_template = environment.get_template("topic_chat.html.jinja2")
 
-# TODO: I think we need to use multiple template file instead of handling it like Blogger's case
-
 with open("./rendered/index.html", mode="w+", encoding="utf-8") as f:
-    f.write(stream_list_template.render())
+    f.write(stream_list_template.render(base_url=BASE_URL))
 with open("./rendered/stream.html", mode="w+", encoding="utf-8") as f:
-    f.write(topic_list_template.render())
+    f.write(topic_list_template.render(base_url=BASE_URL))
 with open("./rendered/topic.html", mode="w+", encoding="utf-8") as f:
-    f.write(topic_chat_template.render())
+    f.write(topic_chat_template.render(base_url=BASE_URL))
 
 
 # Copy needed assets to /rendered
