0ef4e8b6e7b109c6da12e06c43eeba02d94794ba
Rafid Aslam (refeed) <rafidteam@gmail.com>
2022-08-08 15:23:56 +0700

    Template-ize the page title

--- render.py
+++ render.py
@@ -4,18 +4,36 @@ import shutil
 from jinja2 import Environment, FileSystemLoader
 
 BASE_URL = os.getenv("BASE_URL", "")
+INSTANCE_TITLE = "Lorem Ipsum Community Archive"
 
 environment = Environment(loader=FileSystemLoader("templates"))
 stream_list_template = environment.get_template("stream_list.html.jinja2")
 topic_list_template = environment.get_template("topic_list.html.jinja2")
 topic_chat_template = environment.get_template("topic_chat.html.jinja2")
 
+
 with open("./rendered/index.html", mode="w+", encoding="utf-8") as f:
-    f.write(stream_list_template.render(base_url=BASE_URL))
+    f.write(
+        stream_list_template.render(
+            base_url=BASE_URL, page_title=INSTANCE_TITLE, title=INSTANCE_TITLE
+        )
+    )
 with open("./rendered/stream.html", mode="w+", encoding="utf-8") as f:
-    f.write(topic_list_template.render(base_url=BASE_URL))
+    f.write(
+        topic_list_template.render(
+            base_url=BASE_URL,
+            page_title=f"# lorem ipsum | {INSTANCE_TITLE}",
+            title=INSTANCE_TITLE,
+        )
+    )
 with open("./rendered/topic.html", mode="w+", encoding="utf-8") as f:
-    f.write(topic_chat_template.render(base_url=BASE_URL))
+    f.write(
+        topic_chat_template.render(
+            base_url=BASE_URL,
+            page_title=f"Le fishe swimming in a bathtub - # lorem ipsum | {INSTANCE_TITLE}",
+            title=INSTANCE_TITLE,
+        )
+    )
 
 
 # Copy needed assets to /rendered
